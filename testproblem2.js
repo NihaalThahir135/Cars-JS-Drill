40e3f6176805cf9cba28d458a46493fb21075294
NihaalThahir135 <106650531+NihaalThahir135@users.noreply.github.com>
2022-06-06 10:44:04 +0530

    Add files via upload

---
+++ testproblem2.js
@@ -0,0 +1,9 @@
+let inventory = require('../problem/inventory.js');
+let problem2 = require('../problem/problem2');
+const result = problem2(inventory);
+const finalResult = "Last car is a Lincoln Town Car";
+if(result === finalResult){ 
+    console.log(result);
+}else{
+console.log("Results Didn't Match");
+}
