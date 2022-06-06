d6ad2ea054d5ddebe14ad9842f7017d3123f83dc
NihaalThahir135 <106650531+NihaalThahir135@users.noreply.github.com>
2022-06-06 10:43:26 +0530

    Add files via upload

---
+++ testproblem1.js
@@ -0,0 +1,9 @@
+let inventory = require('../problem/inventory.js'); 
+let prob1 = require('../problem/problem1.js'); 
+const testId = 33; 
+const result = prob1(inventory, testId); 
+const finalResult = "Car 33 is a 2011 Jeep Wrangler"; 
+if(result === finalResult){ 
+    console.log(result); 
+}
+else console.log("Results Didn't Match");
